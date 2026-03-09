1e0060b23b5619183a472e47938d169f5547495e
pradheepa jaya shree <pradheepajayashree@gmail.com>
2026-03-09 14:50:46 +0530

    added readme

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Displaying the Welcome Message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+        // Displaying the Application Version
+        System.out.println("Version : 1.0");
+
+        // Indicating successful initialization
+        System.out.println("System initialized successfully.");
+
+    }
+}
